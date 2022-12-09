10be5c3aa56c0b7500c902437eaa30a185806dc0
minjongbaek <minjongbaek@gmail.com>
2022-12-09 23:52:27 +0900

    fix: add API_KEY

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,8 +1,9 @@
 const API_END_POINT = process.env.API_END_POINT
+const API_KEY = process.env.API_KEY
 
 export const request = async (url) => {
   try {
-    const res = await fetch(`${API_END_POINT}${url}`)
+    const res = await fetch(`${API_END_POINT}?apikey=${API_KEY}&${url}`)
 
     if (!res.ok) {
       throw new Error('API Call Error')
