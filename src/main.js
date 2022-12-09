147a2159b37096bce470e6c29957170696862eb9
minjongbaek <minjongbaek@gmail.com>
2022-12-09 23:52:11 +0900

    chore: remove pinia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App'
 import router from './routes'
 
 createApp(App)
-  .use(createPinia())
   .use(router)
   .mount('#app')
