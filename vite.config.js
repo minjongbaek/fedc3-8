bd1a06d65f42b4d990622fb9ae53bffdd414d325
minjongbaek <minjongbaek@gmail.com>
2022-12-14 21:43:53 +0900

    refector: remove loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
 
 
-export default defineConfig(({ mode }) => {
-  const { VITE_API_END_POINT, VITE_API_KEY } = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '~': fileURLToPath(new URL('./src', import.meta.url)),
-      }
-    },
-    define: {
-      API_END_POINT: JSON.stringify(VITE_API_END_POINT), 
-      API_KEY: JSON.stringify(VITE_API_KEY)
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
     }
   }
 })
+
